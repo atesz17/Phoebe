8d630fe3939c1024b9f92579b050d83015d8371c
Bence <bence.demko@gmail.com>
2015-04-25 13:37:27 +0200

    masterbe huzas prototypebol

---
+++ src/com/gto/phoebe/ui/GraphicInterface.java
@@ -0,0 +1,22 @@
+package com.gto.phoebe.ui;
+
+import com.gto.phoebe.domain.Movement;
+import com.gto.phoebe.domain.TrapTypes;
+import com.gto.phoebe.logic.TrapperRobot;
+
+public class GraphicInterface implements UserInterface {
+    @Override
+    public Movement getMovementInput(TrapperRobot robot) {
+        return null;
+    }
+
+    @Override
+    public void print(String message) {
+
+    }
+
+    @Override
+    public TrapTypes getTrapInput(TrapperRobot robot) {
+        return null;
+    }
+}
